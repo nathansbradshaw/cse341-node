a099275f757adf3f3a9fa19390eeffb08d7f33e0
Nathan Bradshaw <nathansbradshaw@gmail.com>
2021-01-09 16:09:26 -0700

    Prove01 and livereload
    Complete the assignment prove 01
    add livereload

--- cse341-project-master/index.js
+++ cse341-project-master/index.js
@@ -14,17 +14,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const livereload = require("livereload");                 //liveReload
+const connectLivereload = require("connect-livereload");  //Make liveReload work on port
 const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+const publicDirectory = path.join(__dirname, 'public');   //
+ 
+//Start liveReload
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(publicDirectory);
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 
 const app = express();
+
+
 
 // Route setup. You can implement more in the future!
 const ta01Routes = require('./routes/ta01');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03'); 
-const ta04Routes = require('./routes/ta04'); 
+const ta04Routes = require('./routes/ta04');
+const prove01Routes = require('./routes/prove01'); 
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(publicDirectory))
+   .use(connectLivereload())  //enable live reload
    .set('views', path.join(__dirname, 'views'))
    .set('view engine', 'ejs')
    // For view engine as Pug
@@ -33,10 +50,12 @@ app.use(express.static(path.join(__dirname, 'public')))
    //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
    //.set('view engine', 'hbs')
    .use(bodyParser({extended: false})) // For parsing the body of a POST
+   .use(bodyParser.urlencoded({extended: false})) // For parsing the body of a POST
    .use('/ta01', ta01Routes)
    .use('/ta02', ta02Routes) 
    .use('/ta03', ta03Routes) 
    .use('/ta04', ta04Routes)
+   .use('/prove01', prove01Routes)
    .get('/', (req, res, next) => {
      // This is the primary index, always handled last. 
      res.render('pages/index', {title: 'Welcome to my CSE341 repo', path: '/'});
---
+++ cse341-project-master/routes/prove01.js
@@ -0,0 +1,26 @@
+//Pove01
+const express = require('express');
+const router = express.Router();
+// const path = require('path');
+
+router.get('/',(req, res, next) => {
+    res.render('pages/prove01_form', { 
+        title: 'Prove01', 
+        path: '/prove01_form', // For pug, EJS 
+      //   activeTA03: true, // For HBS
+        contentCSS: true, // For HBS
+    });
+});
+
+router.post('/', (req, res, next) => {
+    console.log(req.body);
+    res.render('pages/prove01_response', { 
+      title: 'Prove01', 
+      path: '/prove01_response', // For pug, EJS 
+    //   activeTA03: true, // For HBS
+      contentCSS: true, // For HBS
+      data: req.body,
+  });
+});
+
+module.exports = router;
